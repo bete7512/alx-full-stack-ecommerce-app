3669fe26b6aa526f78c81346ae16545fa930d6c9
bete7512 <betekbebe@gmail.com>
2022-11-09 11:00:44 +0300

    change commited succefully

--- frontend/client/src/components/common/Nav.vue
+++ frontend/client/src/components/common/Nav.vue
@@ -3,7 +3,7 @@
         <div class="flex justify-between items-center text-slate-900 bg-[#6DB0B0]  h-20">
             <div class="text-4-xl font-bold">BS</div>
             <div>
-                <div>Categories sv</div>
+                <div>Categories</div>
             </div>
             <div></div>
         </div>
