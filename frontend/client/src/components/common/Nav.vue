47f0cb9c49abaa28cc338f1612544407a2315677
bete7512 <betekbebe@gmail.com>
2022-11-09 10:04:35 +0300

    change commited succefully

--- frontend/client/src/components/common/Nav.vue
+++ frontend/client/src/components/common/Nav.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <div class="flex justify-between items-center text-slate-900 bg-emerald-600">
-            <div ></div>
+        <div class="flex justify-between items-center text-slate-900 bg-emerald-600 h-20">
+            <div class="text-4-xl font-bold">BS</div>
             <div></div>
             <div></div>
         </div>
--- frontend/client/src/pages/Home.vue
+++ frontend/client/src/pages/Home.vue
@@ -1,6 +1,6 @@
 <template >
     <div>
-        
+        hello
     </div>
 </template>
 <script setup>
