766289a43de02978703a1d3563438a65b57175c9
bete7512 <betekbebe@gmail.com>
2022-11-10 09:01:15 +0300

    change commited succefully

--- frontend/delivery-agent/src/router/index.js
+++ frontend/delivery-agent/src/router/index.js
@@ -1,13 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import Home from '../pages/Home.vue'
+import Login from '../pages/Login.vue'
+import Signup from '../pages/Signup.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: HomeView
-    // },
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+      meta:{layout:'empty'}
+    },  
+      {
+      path: '/login',
+      name: 'home',
+      component: Home,
+      meta:
+    },
 
   ]
 })
