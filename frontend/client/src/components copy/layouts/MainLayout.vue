57cde54da13671003f425454d3c397c89eb29507
bete7512 <betekbebe@gmail.com>
2022-11-10 08:54:22 +0300

    change commited succefully

---
+++ frontend/client/src/components copy/layouts/MainLayout.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+       <Nav class="lg:block hidden"></Nav>
+       <Mobile class="block sm:hidden"></Mobile>
+        <slot></slot>
+    </div>
+</template>
+<script setup>
+import Nav from '../common/Nav.vue';
+import Mobile from '../common/Mobile/Nav.vue';
+</script>
+<style>
+    
+</style>
--- frontend/delivery-agent/src/App.vue
+++ frontend/delivery-agent/src/App.vue
@@ -1,5 +1,5 @@
 <template>
- <div class="text-red-800">SOmething</div>
+ 
 </template>
 <script setup>
 </script>
