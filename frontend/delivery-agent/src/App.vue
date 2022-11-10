d571aea8b0c1176c5eb7e3294a36886eb63b6960
bete7512 <betekbebe@gmail.com>
2022-11-10 09:06:25 +0300

    change commited succefully

--- frontend/delivery-agent/src/App.vue
+++ frontend/delivery-agent/src/App.vue
@@ -1,8 +1,22 @@
 <template>
- <router-view></router-view>
-</template>
-<script setup>
-</script>
-<style>
-
-</style>
+    <component :is="layout">
+      <router-view></router-view>
+    </component>
+  </template>
+  <script setup>
+  import { computed } from 'vue'
+  import { useRouter } from 'vue-router'
+  const { currentRoute } = useRouter()
+  const defaultlayout = 'empty'
+  console.log()
+  const layout = computed(
+    () => `${currentRoute.value.meta.layout || defaultlayout}-layout`
+  )
+  console.log(layout)
+  </script>
+  
+  
+  <style>
+  
+  </style>
+  
--- frontend/delivery-agent/src/main.js
+++ frontend/delivery-agent/src/main.js
@@ -3,9 +3,13 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import Main from './components/layouts/MainLayout.vue'
+import Empty from './components/layouts/EmptyLayout.vue'
 import './index.css'
 
 const app = createApp(App)
+app.component('main-layout',Main)
+app.component('empty-layout',Empty)
 
 app.use(createPinia())
 app.use(router)
