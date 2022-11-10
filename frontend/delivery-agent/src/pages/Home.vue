6e352e3fa5b0a0e5860b92d57e63125b83f8607b
bete7512 <betekbebe@gmail.com>
2022-11-10 09:25:22 +0300

    change commited succefully

--- frontend/delivery-agent/src/pages/Home.vue
+++ frontend/delivery-agent/src/pages/Home.vue
@@ -1,11 +1,9 @@
 <template >
-    <div class="sm:flex block px-5 w-full h-screen items-center justify-center">
+    <div class="sm:flex block px-5 w-full h-screen items-start pt-10 justify-center">
         <div class="w-1/2 text-8xl  font-bold">
             Deliver Fast and get more  hunt for work on our platform.
         </div>
-        <div>
-            <img class="w-1/2 " src="../assets/delivery.jpeg" alt="">
-        </div>
+            <img class="w-1/2 h-1/2 " src="../assets/delivery.jpeg" alt="">
     </div>
 </template>
 <script setup>
