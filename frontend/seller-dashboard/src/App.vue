307707f8aa85187267f15127d0b405f8d3ca5735
bete7512 <betekbebe@gmail.com>
2022-11-11 09:08:35 +0300

    change commited succefully

--- frontend/seller-dashboard/src/App.vue
+++ frontend/seller-dashboard/src/App.vue
@@ -7,7 +7,11 @@
 import { RouterView, RouterLink } from 'vue-router'
 import {ref,computed} from  'vue'
 import { useRoute } from 'vue-router'
-const 
+const layout = computed(() => {
+  const route = useRoute()
+  const layout = `${route.meta.layout ||  "empty"}`
+  return layout
+})
 </script>
 
 <style>
