1206fc05a65f956123d420f88035a1e50842aeb1
bete7512 <betekbebe@gmail.com>
2022-11-11 10:40:20 +0300

    change commited succefully

---
+++ frontend/super-admin-dashboard/src/components/layouts/MainLayout.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+       <Nav>
+         <slot></slot>
+       </Nav>
+    </div>
+</template>
+<script setup>
+import Nav from '../common/Nav.vue';
+</script>
+<style>
+    
+</style>
--- frontend/super-admin-dashboard/src/main.js
+++ frontend/super-admin-dashboard/src/main.js
@@ -4,11 +4,14 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import Main from './components/layouts/MainLayout.vue'
+import Empty from './components/layouts/EmptyLayout.vue'
+import './index.css'
 
 const app = createApp(App)
+app.component('main-layout',Main)
+app.component('empty-layout',Empty)
 
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
