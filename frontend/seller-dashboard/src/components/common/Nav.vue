3311758dac73ac58035050d7ff2ea67601a13ead
bete7512 <betekbebe@gmail.com>
2022-11-11 09:39:29 +0300

    change commited succefully

--- frontend/seller-dashboard/src/components/common/Nav.vue
+++ frontend/seller-dashboard/src/components/common/Nav.vue
@@ -4,13 +4,14 @@
         </div>
         <div class="w-full">
             <div class="w-full text-3xl items-center pt-0 h-20 bg-slate-200 flex justify-between">
-                <div class="sm:hidden block">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="42" height="42" fill="currentColor"
-                        class="bi bi-list" viewBox="0 0 16 16">
+                <div class="">
+                    <svg  xmlns="http://www.w3.org/2000/svg" width="42" height="42" fill="currentColor"
+                        class="bi bi-list sm:hidden block" viewBox="0 0 16 16">
                         <path fill-rule="evenodd"
                             d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
                     </svg>
                 </div>
+                
                 <div class="flex space-x-3 ">
                     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="red" class="bi bi-bell"
                         viewBox="0 0 16 16">
