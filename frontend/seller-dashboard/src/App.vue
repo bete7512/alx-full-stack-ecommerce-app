02299f3f410e3c378a0fd068e3ee87635aa2ddb6
bete7512 <betekbebe@gmail.com>
2022-11-11 09:10:18 +0300

    change commited succefully

--- frontend/seller-dashboard/src/App.vue
+++ frontend/seller-dashboard/src/App.vue
@@ -4,14 +4,23 @@
   </component>
 </template>
 <script setup>
-import { RouterView, RouterLink } from 'vue-router'
-import {ref,computed} from  'vue'
-import { useRoute } from 'vue-router'
-const layout = computed(() => {
-  const route = useRoute()
-  const layout = `${route.meta.layout ||  "empty"}`
-  return layout
-})
+// import { RouterView, RouterLink } from 'vue-router'
+// import {ref,computed} from  'vue'
+// import { useRoute } from 'vue-router'
+// const layout = computed(() => {
+//   const route = useRoute()
+//   const layout = `${route.meta.layout ||  "empty"}-layout`
+//   return layout
+// })
+
+import { computed } from 'vue'
+import { useRouter } from 'vue-router'
+const { currentRoute } = useRouter()
+const defaultlayout = 'empty'
+console.log()
+const layout = computed(
+  () => `${currentRoute.value.meta.layout || defaultlayout}-layout`
+)
 </script>
 
 <style>
