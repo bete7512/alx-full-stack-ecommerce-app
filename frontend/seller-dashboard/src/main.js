38ee9adb051a3c40af7c32f8098d407be9e9b9d0
bete7512 <betekbebe@gmail.com>
2022-11-11 09:01:44 +0300

    change commited succefully

--- frontend/seller-dashboard/src/main.js
+++ frontend/seller-dashboard/src/main.js
@@ -4,11 +4,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+// import './assets/main.css'
+import './index.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
 app.mount('#app')
---
+++ frontend/seller-dashboard/src/pages/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        hello home
+    </div>
+</template>
+<script setup>
+
+</script>
+<style lang="">
+    
+</style>
