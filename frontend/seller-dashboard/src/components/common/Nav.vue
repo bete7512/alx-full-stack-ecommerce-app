a6b4d0ffeb6cd0b8f747028ca7a8a3299969a08a
bete7512 <betekbebe@gmail.com>
2022-11-11 09:32:37 +0300

    change commited succefully

--- frontend/seller-dashboard/src/components/common/Nav.vue
+++ frontend/seller-dashboard/src/components/common/Nav.vue
@@ -3,11 +3,11 @@
         <div class="w-64 sm:block hidden bg-orange-300 h-screen ">
         </div>
         <div class="w-full">
-            <div class="w-full text-3xl pt-0 h-20 bg-slate-200">
+            <div class="w-full text-3xl pt-0 h-20 bg-slate-200 flex justify-between">
+
             </div>
             <slot></slot>
         </div>
-
     </div>
 </template>
 <script setup>
