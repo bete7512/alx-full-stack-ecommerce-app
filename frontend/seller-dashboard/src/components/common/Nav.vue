c926f78b959f5b2f5828e22348225d1dd4aa0a4f
bete7512 <betekbebe@gmail.com>
2022-11-11 09:29:10 +0300

    change commited succefully

--- frontend/seller-dashboard/src/components/common/Nav.vue
+++ frontend/seller-dashboard/src/components/common/Nav.vue
@@ -2,10 +2,13 @@
     <div class="space-x-0 flex">
         <div class="w-1/6 bg-orange-300 h-screen ">
         </div>
-        <div class="w-full text-3xl pt-0 h-20 bg-slate-200">
+        <div>
+            <div class="w-full text-3xl pt-0 h-20 bg-slate-200">
+            </div>
+            <slot></slot>
         </div>
-    </div>
 
+    </div>
 </template>
 <script setup>
 
