a53fbad4ef351060a9b98ca8582650a6b40eb567
bete7512 <betekbebe@gmail.com>
2022-11-11 10:20:12 +0300

    change commited succefully

--- frontend/seller-dashboard/src/components/common/Nav.vue
+++ frontend/seller-dashboard/src/components/common/Nav.vue
@@ -1,7 +1,7 @@
 <template>
+    <Mdrawer class="sm:hidden block" v-if="isNav" v-on:close="isNav = false"></Mdrawer>
     <div class="space-x-0 flex">
        <Drawer class="sm:block hidden"></Drawer>
-       <Drawer class="sm:hidden block" v-if="isNav" v-on:close="isNav = false"></Drawer>
         <div class="w-full">
             <div class="w-full text-3xl px-5 items-center pt-0 h-20 bg-slate-200 flex justify-between">
                 <div class="">
@@ -33,6 +33,7 @@
 </template>
 <script setup>
 import Drawer from './Drawer.vue';
+import Mdrawer from './Mobile/Drawer.vue'
 import {ref} from 'vue'
 const isNav = ref(false)
 const nav = ()=>{
