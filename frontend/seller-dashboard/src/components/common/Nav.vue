78d3fcf325b3a76213b7a21eee4b15b8b521e93e
bete7512 <betekbebe@gmail.com>
2022-11-11 09:53:08 +0300

    change commited succefully

--- frontend/seller-dashboard/src/components/common/Nav.vue
+++ frontend/seller-dashboard/src/components/common/Nav.vue
@@ -1,15 +1,18 @@
 <template>
     <div class="space-x-0 flex">
        <Drawer class="sm:block hidden"></Drawer>
-       <Drawer v-if="isNav"></Drawer>
+       <Drawer v-if="isNav" v-on:close="isNav = f"></Drawer>
         <div class="w-full">
             <div class="w-full text-3xl px-5 items-center pt-0 h-20 bg-slate-200 flex justify-between">
                 <div class="">
-                    <svg  xmlns="http://www.w3.org/2000/svg" width="42" height="42" fill="currentColor"
-                        class="bi bi-list sm:hidden block" viewBox="0 0 16 16">
+                    <button @click="" class="sm:hidden block">
+                         <svg  xmlns="http://www.w3.org/2000/svg" width="42" height="42" fill="currentColor"
+                        class="bi bi-list " viewBox="0 0 16 16">
                         <path fill-rule="evenodd"
                             d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
                     </svg>
+                    </button>
+                   
                 </div>
                 <div class="flex items-center space-x-3 ">
                     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="red" class="bi bi-bell"
@@ -30,7 +33,8 @@
 </template>
 <script setup>
 import Drawer from './Drawer.vue';
-import {ref} from 
+import {ref} from 'vue'
+const isNav = ref(false)
 </script>
 <style lang="">
     
