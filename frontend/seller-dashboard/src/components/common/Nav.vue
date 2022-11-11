b49f1d0e2396c4300fba5c659af3a4edc7bb3cc0
bete7512 <betekbebe@gmail.com>
2022-11-11 09:42:53 +0300

    change commited succefully

--- frontend/seller-dashboard/src/components/common/Nav.vue
+++ frontend/seller-dashboard/src/components/common/Nav.vue
@@ -3,7 +3,7 @@
         <div class="w-64 sm:block hidden bg-orange-300 h-screen ">
         </div>
         <div class="w-full">
-            <div class="w-full text-3xl items-center pt-0 h-20 bg-slate-200 flex justify-between">
+            <div class="w-full text-3xl px-5 items-center pt-0 h-20 bg-slate-200 flex justify-between">
                 <div class="">
                     <svg  xmlns="http://www.w3.org/2000/svg" width="42" height="42" fill="currentColor"
                         class="bi bi-list sm:hidden block" viewBox="0 0 16 16">
@@ -11,8 +11,7 @@
                             d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
                     </svg>
                 </div>
-                
-                <div class="flex space-x-3 ">
+                <div class="flex items-center space-x-3 ">
                     <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="red" class="bi bi-bell"
                         viewBox="0 0 16 16">
                         <path
@@ -23,7 +22,6 @@
                         <path
                             d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
                     </svg>
-
                 </div>
             </div>
             <slot></slot>
