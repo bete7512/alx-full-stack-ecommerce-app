c7adf1f39199587e6d4668b21604e59919a2a0c1
bete7512 <betekbebe@gmail.com>
2022-11-11 09:20:35 +0300

    change commited succefully

--- frontend/seller-dashboard/src/components/common/Nav.vue
+++ frontend/seller-dashboard/src/components/common/Nav.vue
@@ -1,6 +1,14 @@
 <template>
-    <div class="w-full text-3xl pt-0 h-20 bg-teal-600">
+    <div>
+        <div class="w-1/ h-screen ">
+
+        </div>
+        <div>
+            <div class="w-full text-3xl pt-0 h-20 bg-teal-600">
+            </div>
+        </div>
     </div>
+
 </template>
 <script setup>
 
