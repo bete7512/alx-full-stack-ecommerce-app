78b74f4f07412b644001b06129f9f23ce318ec2d
bete7512 <betekbebe@gmail.com>
2022-11-15 14:59:10 +0300

    change commited succefully

--- frontend/seller-dashboard/src/pages/Products/ProductDetail.vue
+++ frontend/seller-dashboard/src/pages/Products/ProductDetail.vue
@@ -1,11 +1,13 @@
 <template>
     <div>
         <div
-        class="overlay px-10 sm:px-0  sm:py-0 py-2 z-50 top-0 left-0 bottom-0 right-0 flex items-center justify-center">
-        <div class="space-y-4  sm:h-3/4 h-full sm:w-1/2 w-full  bg-white rounded-md">
-            <div class="flex  justify-between items-center px-4 pt-3 ">
+            class="overlay px-10 sm:px-0  sm:py-0 py-2 z-50 top-0 left-0 bottom-0 right-0 flex items-center justify-center">
+            <div class="space-y-4  auto sm:w-1/2 w-full  bg-white rounded-md">
+                <div v-if="error">error in fetching products</div>
+                <div v-else-if="loading">loading Please Wait</div>
                 
-    </div>
+            </div>
+        </div>
     </div>
 </template>
 <script setup>
@@ -39,5 +41,5 @@ const products = ProductStore();
 const product = computed(() => result.value?.product ?? []);
 </script>
 <style>
-    
+
 </style>
