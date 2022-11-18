653bf47b42e4758e36a9688e91c2fc8f719a9e8e
bete7512 <betekbebe@gmail.com>
2022-11-18 19:24:34 +0300

    change commited succefully

--- backend/express/server.js
+++ backend/express/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 require('dotenv').config()
 const app = express();
 app.use(express.json({ limit: '200mb' }));
-// app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({extended:true}));
 // app.get('/sol',(req,res)=>{
 //   res.send({
 //     message:"hello sol"
