a25d0d35145d3aa0ef666bb860e7dcf06cc495b7
Solomon Legesse <slegesse2444@gmail.com>
2022-12-07 15:13:22 +0300

    Update Footer.vue

--- frontend/client/src/components/common/Footer.vue
+++ frontend/client/src/components/common/Footer.vue
@@ -13,7 +13,7 @@
                 <div>+251962247109</div>
             </div>
             <div class="font-bold">
-                <div>Subscribe ower New letter</div>
+                <div>Subscribe our News letter</div>
                 <div class="rounded-full w-80 h-10 bg-red-700 flex "><input type="text" class="w-48 rounded-full"><button w->subscribe</button></div>
             </div>
         </div>
